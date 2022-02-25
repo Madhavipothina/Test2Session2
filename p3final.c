a0c11de881685b78b2a1211cc35c7f5e2e9f5332
Venu Madhavi <94669721+Madhavipothina@users.noreply.github.com>
2022-02-25 09:58:24 +0000

    p3fiinal

--- p3final.c
+++ p3final.c
@@ -1,1 +1,33 @@
-
+#include<stdio.h>
+#include<math.h>
+int input_number()
+{
+  int a;
+  printf("enter the number\n");
+  scanf("%d",&a);
+  return a;
+}
+int is_prime(int n)
+{
+  int i;
+  for(i=2;i<sqrt(n);i++)
+  if(n%i==0)
+    return 0;
+  else
+   return 1;
+}
+void output(int n,int isprime)
+{
+  if(isprime==0)
+   printf("the number is composite\n");
+ else
+   printf("the number is prime\n");  
+}
+int main()
+{
+  int n, isprime;
+  n=input_number();
+  isprime=is_prime(n);
+  output(n,isprime);
+  return 0;
+}
